a83fef108588bc83cc6cbf6906af805f0677150a
Nika Yovbak <93946871+tiastaa@users.noreply.github.com>
2022-11-13 22:46:54 +0200

    Create program.cs

---
+++ lab6/program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Site site = new Site();
+            site.Display();
+            site.LogIn();
+            site.LogIn(); // already logged
+            site.LogOut();
+            site.LogIn();
+
+            site.AddToBasket("short");
+            
+        }
+    }
+}
